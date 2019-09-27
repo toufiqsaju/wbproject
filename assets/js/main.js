b53a1ceceb349058843df552f851d2261baac127
toufiqsaju <toufiqulislam2503@gmail.com>
2019-09-28 01:47:37 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	
+  $(".slider-area").owlCarousel({
+	  items:1,
+	  autoplay:false,
+	  dots:true,
+	  nav:true,
+	  loop:true,
+	  navText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+	 
+	  
+  });
+});
